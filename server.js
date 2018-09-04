ad19f8cd5274d5c501a5fd7a1f05631472146528
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-04 14:47:29 -0400

    Set up GET /api/notes

--- server.js
+++ server.js
@@ -3,14 +3,15 @@
 // Load array of notes
 const data = require('./db/notes');
 
-console.log('Hello Noteful!');
-
 // INSERT EXPRESS APP CODE HERE...
-
 const express = require('express');
-
 const app = express();
+
 app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+  res.json(data);
+});
 
 app.listen(8080, function() {
   console.info(`Server listening on ${this.address().port}`);
