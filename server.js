4079676d641ab51b40353d690824cd3905cff8ec
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-04 15:11:39 -0400

    Get /api/notes/:id

--- server.js
+++ server.js
@@ -10,7 +10,12 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
-  res.json(data);
+  return res.json(data);
+});
+
+app.get('/api/notes/:id', (req, res) => {
+  const id = req.params.id;
+  return res.json(data.find(item => item.id === Number(id)));
 });
 
 app.listen(8080, function() {
