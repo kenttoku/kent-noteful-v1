afa922bd2f71ef6e2e5bb5bd630a84fd0b641c0c
Kent Tokunaga <kenttokunaga@gmail.com>
2018-09-05 15:48:45 -0400

    Add PUT /api/notes/:id

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use(logger);
 app.use(express.static('public'));
+app.use(express.json());
 
 app.get('/api/notes', (req, res, next) => {
   const { searchTerm } = req.query;
@@ -33,6 +34,31 @@ app.get('/api/notes/:id', (req, res, next) => {
       return next(err);
     }
     res.json(note);
+  });
+});
+
+app.put('/api/notes/:id', (req, res, next) => {
+  const id = req.params.id;
+
+  /***** Never trust users - validate input *****/
+  const updateObj = {};
+  const updateFields = ['title', 'content'];
+
+  updateFields.forEach(field => {
+    if (field in req.body) {
+      updateObj[field] = req.body[field];
+    }
+  });
+
+  notes.update(id, updateObj, (err, item) => {
+    if (err) {
+      return next(err);
+    }
+    if (item) {
+      res.json(item);
+    } else {
+      next();
+    }
   });
 });
 
